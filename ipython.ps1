d4d81819c2beb308cab2ab2bab2d2c4e14a81bc7
windviki <windviki@gmail.com>
2020-07-21 17:57:04 +0800

    Add ipython support.

---
+++ ipython.ps1
@@ -0,0 +1,33 @@
+## PVM: Python version manager
+## by viki
+## windviki@gmail.com
+##
+## See readme.md!
+##
+$PVM_SCRIPT_ROOT = ""
+Try
+{
+    $PVM_SCRIPT_ROOT = Get-Variable -Name PSScriptRoot -ValueOnly -ErrorAction Stop
+}
+Catch
+{
+    $PVM_SCRIPT_ROOT = Split-Path $script:MyInvocation.MyCommand.Path
+}
+
+$scriptPath = Join-Path $PVM_SCRIPT_ROOT "__pvm_do_not_use_it_directly.ps1"
+$argumentList = $PsBoundParameters.Values + $args
+$processedArgs = [System.Collections.ArrayList]@()
+for($i=0; $i -lt $argumentList.Count; $i++)
+{
+    # Write-Host "Arguments $i : " $argumentList[$i]
+    if ($argumentList[$i] -match "\s")
+    {
+        $null = $processedArgs.Add('"{0}"' -f $argumentList[$i])
+    }
+    else
+    {
+        $null = $processedArgs.Add($argumentList[$i])
+    }
+}
+# Write-Host "Input argumentList: $processedArgs"
+$env:PVM_TARGET_APP = 'ipython'; Invoke-Expression "& `"$scriptPath`" $processedArgs"
